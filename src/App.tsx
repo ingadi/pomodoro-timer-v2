bb4421f49c81d405e4f362aa3bbbc2a2af0e4f00
ingadi <axelingadi@gmail.com>
2023-04-25 13:55:54 +0300

    Remove background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
 export default function App() {
-  return (
-    <main
-      className="pattern-dots pattern-blue-500 pattern-bg-white dark:pattern-bg-slate-950 
-      pattern-size-6 pattern-opacity-20 w-screen h-screen grid place-content-center"
-    ></main>
-  );
+  return <main className="w-screen h-screen grid place-content-center"></main>;
 }
