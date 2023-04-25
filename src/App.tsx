ba7093ee96c4242f9534b9f92dd9d6fd2b322b5e
ingadi <axelingadi@gmail.com>
2023-04-25 13:20:43 +0300

    Change pattern to dots

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <main
-      className="pattern-cross pattern-blue-500 pattern-bg-white dark:pattern-bg-slate-950 
+      className="pattern-dots pattern-blue-500 pattern-bg-white dark:pattern-bg-slate-950 
       pattern-size-6 pattern-opacity-20 w-screen h-screen grid place-content-center"
     ></main>
   );
